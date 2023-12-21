9c4010f36b425e0a6eb4045973890c5d0a0f0cd7
Chin P. Ho <newez82@yahoo.com>
2023-12-21 09:44:36 -0800

    added os module

---
+++ os_module/os.py
@@ -0,0 +1,8 @@
+"""
+    underlying operating system functionality
+        - navigate the file system
+        - get file information
+        - lookup and change the enviornment variable
+        - move file around
+"""
+import os
