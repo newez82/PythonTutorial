b0de3c1a11f74ccfa1b305bd42902853ff6b0db6
Chin P. Ho <newez82@yahoo.com>
2024-03-27 11:38:27 -0700

    add logic to find the missing files between 2 directories

--- Exericse/files_reconciliation/webdav_files_reconciliation.py
+++ Exericse/files_reconciliation/webdav_files_reconciliation.py
@@ -9,14 +9,25 @@ import os
 
 from webdav3.client import Client
 
+print("Get Current Directory: ", os.getcwd())
+os.chdir("G:\\PICTURES\\2023_iphoneMaxBackup\\networkTransfer\\NextCloud\\")
+print("Get Changed Directory: ", os.getcwd())
+
+
 options = {
-    "webdav_hostname": "https://192.168.50.162:8283/remote.php/dav/files/cho/Pixel5",
-    "webdav_login": "cho",
-    "webdav_password": "Gre@tR/cl98z",
+    "webdav_hostname": "https://192.168.50.162:8283/remote.php/dav/files/annachow/iPhone13Max",
+    "webdav_login": "annachow",
+    "webdav_password": "6463460876",
     "cert_path": "C:\\Users\\chin.p.ho\\Downloads\\nextcloud.crt",
 }
-
 client = Client(options)
 client.verify = False
 
-print((client.list("Camera/*")))
+print("Total files on NextCloud:", len(client.list()))
+print("Total files on Local Folder:", len(os.listdir()))
+
+setNextCloud = set(client.list())
+setLocal = set(os.listdir())
+
+missing_file = setNextCloud.difference(setLocal)
+print("missing file:", missing_file)
